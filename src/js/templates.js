1da237f6a79f4ac6658c2c91a3411a3d65a84e6c
Nadim Kobeissi <nadim@nadim.computer>
2014-07-28 22:59:27 -0400

    Close #65

--- src/js/templates.js
+++ src/js/templates.js
@@ -1,7 +1,7 @@
 miniLock.templates = {}
 
 miniLock.templates.keyStrengthMoreInfo = ''
-	+ 'The key you have entered is too weak.'
+	+ 'The passphrase you have entered is too weak.'
 	+ '<p>Having a long, unique key is very important for using miniLock. '
 	+ 'Try using a <strong>phrase</strong> that makes sense only to you.<br /><br />'
 	+ 'We strongly recommend allowing miniLock to generate a key for you:<br />'
