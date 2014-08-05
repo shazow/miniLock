shazow/miniLock

commit 4c845591f8a5c8c353d7987d42a3d678a68e6452

Encrypting first chunk length was treated as utf16
Following @dchest advice, fixing #98.

src/js/miniLock.js
@@ -297,14 +297,15 @@ miniLock.crypto.encryptFile = function(
 		fileNonce,
 		miniLock.crypto.chunkSize
 	)
-	var paddedFileName = file.name
-	while (paddedFileName.length < 256) {
-		paddedFileName += String.fromCharCode(0x00)
-	}
+	var paddedFileName = new Uint8Array(256)
+	var fileNameBytes = nacl.util.decodeUTF8(file.name)
+	if (fileNameBytes.length > paddedFileName.length)
+		throw new Error('miniLock: Encryption failed - file name is too long')
+	paddedFileName.set(fileNameBytes)
 	miniLock.session.currentFile.hashObject = new BLAKE2s(32)
 	var encryptedChunk
 	encryptedChunk = miniLock.session.currentFile.streamEncryptor.encryptChunk(
-		nacl.util.decodeUTF8(paddedFileName),
+		paddedFileName,
 		false
 	)
 	if (!encryptedChunk) {

commit 4bfe5978d2c6f6ec41bed0a11cbd45a42fcd4ad1

Fixing broken linting in commit 4c84559

src/js/miniLock.js
@@ -299,8 +299,9 @@ miniLock.crypto.encryptFile = function(
 	)
 	var paddedFileName = new Uint8Array(256)
 	var fileNameBytes = nacl.util.decodeUTF8(file.name)
-	if (fileNameBytes.length > paddedFileName.length)
+	if (fileNameBytes.length > paddedFileName.length) {
 		throw new Error('miniLock: Encryption failed - file name is too long')
+	}
 	paddedFileName.set(fileNameBytes)
 	miniLock.session.currentFile.hashObject = new BLAKE2s(32)
 	var encryptedChunk