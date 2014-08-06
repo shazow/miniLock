72250e931f9ba86c642f3f07f21bde2dd46b7185
Nadim Kobeissi <nadim@nadim.computer>
2014-08-06 18:16:59 -0400

    Descriptions for tests

--- test/tests/UI.getBasenameAndExtensions.js
+++ test/tests/UI.getBasenameAndExtensions.js
@@ -3,16 +3,19 @@ QUnit.test('UI.getBasenameAndExtensions', function(assert) {
 	'use strict';
 	assert.deepEqual(
 		miniLock.UI.getBasenameAndExtensions('SCAN.JPG'),
-		{basename: 'SCAN', extensions: '.JPG'}
+		{basename: 'SCAN', extensions: '.JPG'},
+		'SCAN.JPG'
 	)
 
 	assert.deepEqual(
 		miniLock.UI.getBasenameAndExtensions('Archive.tar.zip'),
-		{basename: 'Archive', extensions: '.tar.zip'}
+		{basename: 'Archive', extensions: '.tar.zip'},
+		'Archive.tar.zip'
 	)
 
 	assert.deepEqual(
-		miniLock.UI.getBasenameAndExtensions('Sombody With A. Period CV.2014.docx.exe.pdf'),
-		{basename: 'Sombody With A. Period CV', extensions: '.2014.docx.exe.pdf'}
+		miniLock.UI.getBasenameAndExtensions('Somebody With A. Period CV.2014.docx.exe.pdf'),
+		{basename: 'Somebody With A. Period CV', extensions: '.2014.docx.exe.pdf'},
+		'Somebody With A. Period CV.2014.docx.exe.pdf'
 	)
 })
