955a34244c42b8716852ac1c9b10f11a1c923fc1
Nadim Kobeissi <nadim@nadim.computer>
2014-08-17 21:35:33 -0400

    Update Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -14,9 +14,9 @@ module.exports = function(grunt) {
 					urls: ["http://127.0.0.1:9999/test/mUTK.html"],
 					build: process.env.TRAVIS_JOB_ID,
 					browsers: [{ browserName: "chrome" }],
-					testname: "miniLock qunit tests",
-					username: "thisisatest",
-					key: "6b4c8849-1434-4ccf-a2bf-796400cbf5cb"
+					testname: "miniLock QUnit Tests",
+					username: "kaepora",
+					key: "131ae777-402f-464c-88e7-6a6e7c8e8727"
 				}
 			}
 		}
